3c2286af5a1bda2b17d9c51899dd6f3670465edd
wnsdh0202 <153583025+wnsdh0202@users.noreply.github.com>
2024-03-25 16:32:04 +0900

    Update backend.tf

--- 04_project/infra/vpc/backend.tf
+++ 04_project/infra/vpc/backend.tf
@@ -1,9 +1,9 @@
 terraform {
     backend "s3" {
-      bucket = "aws05-terraform-state"
+      bucket = "project01-terraform-state"
       region = "ap-northeast-2"
       key = "infra/vpc/terraform.tfstate"
-      dynamodb_table = "aws05-terraform-locks"
+      dynamodb_table = "project01-terraform-locks"
       encrypt  = true
     }
 }
