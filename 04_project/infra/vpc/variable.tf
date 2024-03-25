7c15aa5985a62ce7fdada1d3771cb63c02b8539e
wnsdh0202 <153583025+wnsdh0202@users.noreply.github.com>
2024-03-25 16:33:32 +0900

    Update variable.tf

--- 04_project/infra/vpc/variable.tf
+++ 04_project/infra/vpc/variable.tf
@@ -1,15 +1,16 @@
 variable "vpc_cidr" {
-  default = "10.5.0.0/16"
+  default = "10.1.0.0/16"
 }
 
 variable "public_subnet" {
   type    = list(any)
-  default = ["10.5.0.0/20", "10.5.16.0/20"]
+  # default = ["10.5.0.0/20", "10.5.16.0/20"]
+  default = ["10.1.0.0/20"]
 }
 
 variable "private_subnet" {
   type    = list(any)
-  default = ["10.5.64.0/20", "10.5.80.0/20"]
+  default = ["10.1.64.0/20", "10.1.80.0/20"]
 }
 
 variable "azs" {
