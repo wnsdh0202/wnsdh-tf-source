4fe34a4755b7eb147a144f61f4a17d79d7b1e626
wnsdh0202 <153583025+wnsdh0202@users.noreply.github.com>
2024-03-25 16:33:04 +0900

    Update output.tf

--- 04_project/infra/vpc/output.tf
+++ 04_project/infra/vpc/output.tf
@@ -1,15 +1,15 @@
 output "vpc_id" {
-    value = aws_vpc.aws05-vpc.id
+    value = aws_vpc.project01-vpc.id
 }
 output "public-subnet-2a-id" {
-    value = aws_subnet.aws05-public-subnet-2a.id
+    value = aws_subnet.project01-public-subnet-2a.id
 }
-output "public-subnet-2c-id" {
-    value = aws_subnet.aws05-public-subnet-2c.id
-}
+# output "public-subnet-2c-id" {
+#     value = aws_subnet.aws05-public-subnet-2c.id
+# }
 output "private-subnet-2a-id" {
-    value = aws_subnet.aws05-private-subnet-2a.id
+    value = aws_subnet.project01-private-subnet-2a.id
 }
 output "private-subnet-2c-id" {
-    value = aws_subnet.aws05-private-subnet-2c.id
+    value = aws_subnet.project01-private-subnet-2c.id
 }
